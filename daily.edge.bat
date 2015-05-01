e7f0c7ce88a995c62a5b19d18adafcecbe988fe5
Bill Petti <billpetti@gmail.com>
2015-05-01 06:27:26 -0400

    Batch file
    Batch file for running R script from the command line

---
+++ daily.edge.bat
@@ -0,0 +1,2 @@
+@echo off 
+"C:\Program Files\R\R-3.1.3\bin\R.exe" CMD BATCH "C:\Users\Petti\Box Sync\Default Sync Folder\Baseball Data\Baseball Data\Edge_Percent\edge_percent\edge_per_daily_update.R"
